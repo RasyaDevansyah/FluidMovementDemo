926ce6e77754f69cc94580baa87a97adf71ac024
Rasya Devansyah <1.moment.i.got.hacked@gmail.com>
2024-05-14 17:42:34 +0700

    Basic movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,20 +1,25 @@
 extends CharacterBody3D
 
+#player nodes
 @export var head : Node
+@onready var standing_collision : CollisionShape3D = $standing_collision
+@onready var crouching_collision : CollisionShape3D = $crouching_collision
+@onready var ray_cast_3d : RayCast3D = $RayCast3D
 
+#speed variables
 @export var current_speed : float = 5.0
-
 @export var walking_speed : float = 5.0
 @export var sprinting_speed : float = 8.0
 @export var crouching_speed : float = 3.0
 
+#movement vars
 @export var jump_velocity : float = 4.5
-
-@export var mouse_sens : float = 0.25
-
+var crouching_depth : float = -0.5
 var lerp_speed : float = 10.0
+
+#input variables
 var direction : Vector3
-var crouching_depth = -0.5
+@export var mouse_sens : float = 0.25
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -23,25 +28,33 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
+	#mouse looking
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
 		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-89), deg_to_rad(89) )
 
 func _physics_process(delta):
+	#handle movement state
 	
+	#crouching
 	if Input.is_action_pressed("crouch"):
 		current_speed = crouching_speed
 		head.position.y = lerp(head.position.y, 0.582 + crouching_depth, delta *lerp_speed)
-	else:
+		standing_collision.disabled = true
+		crouching_collision.disabled = false
+	#standing
+	elif !ray_cast_3d.is_colliding():
+		standing_collision.disabled = false
+		crouching_collision.disabled = true
 		head.position.y = lerp(head.position.y, 0.582 , delta * lerp_speed)
 		if Input.is_action_pressed("sprint"):
+			#sprinting
 			current_speed = sprinting_speed
 		else:
+			#walking
 			current_speed = walking_speed
 		
-	
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -50,8 +63,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * lerp_speed)
 	
